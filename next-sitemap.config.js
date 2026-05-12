e93c75d7a2e4416980e9c522c2ce5bbd79d05c70
akv-dev339 <akshayvermaakv07@gmail.com>
2026-05-12 18:06:21 +0530

    updated sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -18,4 +18,22 @@ module.exports = {
       },
     ],
   },
+
+  additionalPaths: async (config) => [
+    await config.transform(config, "/journals"),
+    await config.transform(config, "/journals/1"),
+    await config.transform(config, "/journals/2"),
+    await config.transform(config, "/journals/1/current-issue"),
+    await config.transform(config, "/journals/2/current-issue"),
+    await config.transform(config, "/journals/1/archive"),
+    await config.transform(config, "/journals/2/archive"),
+    await config.transform(config, "/journals/1/editorial"),
+    await config.transform(config, "/journals/2/editorial"),
+    await config.transform(config, "/journals/1/submission"),
+    await config.transform(config, "/journals/2/submission"),
+    await config.transform(config, "/journals/1/author-guidelines"),
+    await config.transform(config, "/journals/2/author-guidelines"),
+    await config.transform(config, "/journals/1/contact"),
+    await config.transform(config, "/journals/2/contact"),
+  ],
 };
